cb00516b45ba542ab12fbb14c190bc963832888a
Richard Norton <rnorton@solfo.com>
2014-10-02 20:23:11 -0700

    Yay, Luhn's alg.

--- src/Cis194/Hw/Week1.hs
+++ src/Cis194/Hw/Week1.hs
@@ -30,6 +30,18 @@ doubleEveryOther xs = reverse $ map (\(i,x) -> if odd i then 2*x else x) $ zip [
 sumDigits :: [Integer] -> Integer
 sumDigits xs = sum $ map (\x -> sum $ toDigits x) xs
 
+
+{-
+Stolen from http://en.wikipedia.org/wiki/Luhn_algorithm :
+1.) From the rightmost digit, which is the check digit, moving left,
+    double the value of every second digit; if the product of this
+    doubling operation is greater than 9 (e.g., 7 × 2 = 14), then sum
+    the digits of the products (e.g., 10: 1 + 0 = 1, 14: 1 + 4 = 5).
+2.) Take the sum of all the digits.
+3.) If the total modulo 10 is equal to 0 (if the total ends in zero)
+    then the number is valid according to the Luhn formula; else it is
+    not valid.
+-}
 validate :: Integer -> Bool
 validate _ = undefined
 
